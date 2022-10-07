a7410d5aca264ad65a87a9be3645d9790ebb73b1
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-10-07 17:39:43 +0700

    Membuat Method 2

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -50,5 +50,24 @@ namespace BubbleSort
             }
             Console.WriteLine("");
         }
+        public void BubbleSortArray()
+        {
+            for (int i =1; i < n; i++) //For n - 1 passes
+            {
+                //Pada pass i, bandingkan n - i elemen pertama dengan elemen selanjutnya
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (a[j] > a[j + 1]) //Jika elemen tidak dalam urutan yang benar
+                    {
+                        //tukar elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
     }
 }
